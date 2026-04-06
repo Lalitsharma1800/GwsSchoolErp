a7d5b272de7bae994a30b11ed6f4991d386ede4a
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-04-07 00:31:37 +0530

    api handling 05 - student data fetching completed

--- src/fetchApi/students/students.js
+++ src/fetchApi/students/students.js
@@ -1,44 +1,48 @@
 import { supabase } from "../../supabase";
 import store from "../../store/store";
 
-// student data with class filter
-export async function searchWithClass(classNumber) {
-const { data, error } = await supabase
-                                  .from('students_basic')
-                                  .select(
-                                    `
-                                    full_name,
-                                    father_name,
-                                    mother_name,
-                                    total_amount,
-                                    paid_amount,
-                                    due_amount,
-                                    ...class_teacher!inner(
-                                      ...classes!inner(
-                                        class_name
-                                      )
+
+class GetStudentData{
+
+  // student data with class filter
+  async getbyClass(class_number = 1){
+        const obj = await supabase
+                                  .from('class_teacher')
+                                  .select(`
+                                    students_basic(
+                                      full_name,
+                                      father_name,
+                                      mother_name,
+                                      Total_Amount,
+                                      Paid_Amount,
+                                      due_amount
+                                    ),
+                                    classes(
+                                      class_number
                                     )
-                                    `,
-                                  )
-                                  .eq(
-                                    'classes.class_number',
-                                    classNumber,
-                                  )
+                                  `)
+                                  .eq('classes.class_number', class_number)
+        const data = obj.data;
+        const error = obj.error;
          if(error || !data){
             if(error.code === ""){
               throw new Error("No Internet..");
             }
-            if(!data){
-              throw new Error("No data found");
+            if(error){
+              throw new Error("There is an issue, please try again later");
             }
-            throw new Error("There is an issue, please try again later");
-         }   
-         return data;
-}
+              throw new Error("No data found");
+         }
+        
 
-// fees details of an student
-export async function  fees_details() {
-      const { data, error } = await supabase
+         console.log(data);   
+         return data;
+  }
+
+// get fees details and basic details for all
+  async ForAll(){
+
+          const { data, error } = await supabase
                               .from('students_basic')
                               .select(`
                                   id,
@@ -50,13 +54,25 @@ export async function  fees_details() {
                                   due_amount,
                                   class_teacher(
                                   classes(
-                                  class_name
+                                  class_number
                               ))
                                   `)
-  if(error){
-    throw error;
+            if(error || !data){
+                      if(error.code === ""){
+                          throw new Error("No Internet Connection, Please Connect to internet")
+                      }
+                     if(error){
+                        throw new Error("There is an issue, please try again later");
+                      }
+                        throw new Error("No data found");
+                }
+            console.log(data);
+            return data;
   }
-  console.log(data)
-  return data;
 }
+
+const getStudentData = new GetStudentData();
+export default getStudentData;
+
+
 
