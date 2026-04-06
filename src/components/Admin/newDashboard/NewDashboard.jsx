c4348028ecbe314c9c95020b5349d3b256c0d9b8
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-04-07 00:29:16 +0530

    experiment for dashboard ui

---
+++ src/components/Admin/newDashboard/NewDashboard.jsx
@@ -0,0 +1,11 @@
+import Navbar2 from "@/components/NavBar/Navbar";
+import HeroSection from "@/components/heroSection/HeroSection";
+export default function NewDashboard(){
+    return(
+        <>
+        <Navbar2/>
+        <HeroSection />
+        </>
+        
+    )
+}
