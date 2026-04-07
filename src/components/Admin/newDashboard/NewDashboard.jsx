3fc06962b2d341b9ce265a0921930c28b7e2f65e
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-04-07 23:33:35 +0530

    feature card

--- src/components/Admin/newDashboard/NewDashboard.jsx
+++ src/components/Admin/newDashboard/NewDashboard.jsx
@@ -1,10 +1,12 @@
 import Navbar2 from "@/components/NavBar/Navbar";
 import HeroSection from "@/components/heroSection/HeroSection";
+import FeatureCard from "@/components/featureCard/FeatureCard";
 export default function NewDashboard(){
     return(
         <>
         <Navbar2/>
         <HeroSection />
+        <FeatureCard />
         </>
         
     )
