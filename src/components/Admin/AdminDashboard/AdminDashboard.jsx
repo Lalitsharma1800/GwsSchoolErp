9bc1fae8cd6f3423c5ade9cea84ed33e22a0bb33
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-04-08 11:58:59 +0530

    dashboard integrated in admin

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,21 +1,28 @@
-import { 
-            examIcon,  feeIcon, stafficon, noticeicon, salleryicon, recordIcon,
-            resulticon, teachericon,  studenticon, calendarIcon,    
-        } from "../../../assets/index";
-import Card from "../Card/Card";
 import Dashboard from "../Dashboard/Dashboard";
 
 
 export default function AdminDashboard(){
 
+    const content = [
+                {title:"Manage Teachers", toLink:"/admin/ManageTeachers"},
+                {title:"Manage Students",toLink:"/admin/ManageStudents"},
+                {title: "HR",toLink:"/admin/HR"},
+                {title: "Manage Accounts",toLink:"/admin/ManageAccounts"}
+    ]
+
 
     return(
-
-
             <>
-            <Dashboard/>
-       
-
+            <Dashboard content = {content}/>
+            {/* footer */}
+        <hr className="mt-10"/>
+            <footer className="h-20 w-full px-10">
+                <ul className="flex justify-between items-center flex-wrap">
+                    <li>Contact</li>
+                    <li>About Us</li>
+                    <li>Privacy-Policy</li>
+                </ul>
+            </footer>
             </>
     )
 }
