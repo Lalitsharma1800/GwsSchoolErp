431c9ed9a8a857b1fe1c07f94166c59b8fba9f28
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-04-09 00:15:12 +0530

    ready to deploy

--- src/Loaders/adminLoader/admin_dashboard_loader.js
+++
@@ -1,3 +0,0 @@
-export default async function admin_dashboard_loader(params) {
-    
-}
--- src/components/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-export default function Loading(){
-    return(
-        <div className="w-full h-screen text-black grid place-content-center">
-           <h1 className="text-2xl"> Loading....</h1>
-            
-           <h3>Please Wait</h3>
-            </div>
-    )
-}
